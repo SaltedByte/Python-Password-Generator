d491951cb19bccc714885ea33dd0f8fccdb585d0
Andrew Lucas <alucas1998@hotmail.com>
2024-07-05 14:18:53 -0400

    Basic functionality

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import random
+
 def main():
-    print("Hello World")
+    passwordLength = 16
+    validChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+    random.shuffle(validChar)
+    newPassword = ""
+    for i in range(passwordLength):
+        newPassword += random.choice(validChar)
+    print(newPassword)
 
 if __name__ == "__main__":
     main()
